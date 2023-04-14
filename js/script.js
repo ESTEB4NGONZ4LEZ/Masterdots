bb309473bdb7ef12d60f1882b420da34966f3189
Esteban Gonzalez <kyosamacps@gmail.com>
2023-04-13 22:28:59 -0500

    Agregado validacion formulario

--- js/script.js
+++ js/script.js
@@ -2,21 +2,24 @@
 const nickInput = document.querySelector('#nick');
 const tamanoInput = document.querySelector('#tamano');
 const formEntrada = document.querySelector('#formEntrada');
+const error = document.querySelector('#error');
 
 //Funciones de evento 
 function comprobarForm(event){
     //Comprobar cambios
-    if (nickInput.value.length == 0)
+    if (nickInput.value.match(/(?<!\S)[0-9]/))
     {
         console.log("No hay Nick");
         nickInput.focus();
         event.preventDefault();
+        error.innerText = "El campo de nick no puede empezar con un numero" 
         return false;
     } else if (tamanoInput.value == "0")
     {
         console.log("No se ha seleccionado el tamano del panel");
         tamanoInput.focus();
         event.preventDefault();
+        error.innerText = "El campo de tamaño del juego no puede estar vacio"
         return false;
     }
     return true;
